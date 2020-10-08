60185aadacba6663441451a0f0404697c042faed
ПАВЛИН ЩЕРЕВ <p.shterev@tu-varna.bg>
2020-10-09 00:33:24 +0300

    hmm

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,24 +33,26 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.pictureBox1.Location = new System.Drawing.Point(53, 30);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(318, 300);
+            this.pictureBox1.Size = new System.Drawing.Size(404, 377);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(147, 366);
+            this.button1.Location = new System.Drawing.Point(130, 451);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -62,7 +64,7 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(592, 366);
+            this.button2.Location = new System.Drawing.Point(591, 451);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(85, 23);
@@ -74,10 +76,10 @@
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.pictureBox2.Location = new System.Drawing.Point(461, 30);
+            this.pictureBox2.Location = new System.Drawing.Point(451, 11);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(316, 300);
+            this.pictureBox2.Size = new System.Drawing.Size(404, 377);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
@@ -85,7 +87,7 @@
             // button3
             // 
             this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(819, 298);
+            this.button3.Location = new System.Drawing.Point(999, 176);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(79, 32);
@@ -96,7 +98,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(853, 180);
+            this.textBox1.Location = new System.Drawing.Point(1033, 58);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(28, 22);
@@ -104,7 +106,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(853, 236);
+            this.textBox2.Location = new System.Drawing.Point(1033, 114);
             this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(28, 22);
@@ -113,7 +115,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(821, 180);
+            this.label1.Location = new System.Drawing.Point(1001, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(14, 17);
             this.label1.TabIndex = 7;
@@ -122,7 +124,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(821, 236);
+            this.label2.Location = new System.Drawing.Point(1001, 114);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(15, 17);
             this.label2.TabIndex = 8;
@@ -130,18 +132,30 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(905, 13);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Location = new System.Drawing.Point(1111, 409);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(417, 287);
             this.richTextBox1.TabIndex = 9;
             this.richTextBox1.Text = "";
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox3.Location = new System.Drawing.Point(1111, 11);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(404, 377);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1325, 450);
+            this.ClientSize = new System.Drawing.Size(1553, 709);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -158,6 +172,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,6 +190,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,17 +18,17 @@ namespace NM {
         Bitmap img;
         Bitmap img_gray;
         private void button1_Click (object sender, EventArgs e) {
-            
+
             //img = new Bitmap("C:\\Users\\Asus\\Pictures\\background\\Wallpaper-Space.jpg");
-            img = new Bitmap (@"C:\Users\Asus\Desktop\gray.png");
-            //img = new Bitmap (@"C:\Users\Asus\Desktop\img.jpg");
+            img = new Bitmap(@"C:\Users\Asus\Desktop\img.jpg");
+            //img = new Bitmap (@"C:\Users\Asus\Desktop\us.png");
             pictureBox1.Image = img;
             button2.Enabled = true;
 
         }
 
         private void button2_Click (object sender, EventArgs e) {
-           img_gray = convertToGray(img);
+            img_gray = convertToGray(img);
             pictureBox2.Image = img_gray;
             button3.Enabled = true;
         }
@@ -44,49 +44,111 @@ namespace NM {
             return result;
         }
 
-        private void pixialization (Bitmap img,int count_rectX,int count_rectY) {
+        private List<int> AverageGray (Bitmap img, int count_rectX, int count_rectY) {
 
-            List<int>[] lists = new List<int>[count_rectX*count_rectY];
+            List<int>[] lists = new List<int>[count_rectX * count_rectY];
             List<int> result = new List<int>();
+
 
             for (int i = 0; i < lists.Length; i++) {
                 lists[i] = new List<int>();
             }
             int x1, y1;
-            int rectX = img.Width / count_rectX;//100
-            int rectY = img.Height / count_rectY;//75
-            for (int x = 0; x < img.Width; x++) {
-                for (int y = 0; y < img.Height; y++) {
+            int rectX = img.Width / count_rectX;
+            int rectY = img.Height / count_rectY;
+
+            for (int x = 0; x < rectX * count_rectX; x++) {
+                for (int y = 0; y < rectY * count_rectY; y++) {
                     Color c = img.GetPixel(x, y);
-                    
                     x1 = x / rectX;
                     y1 = y / rectY;
                     int index = x1 + y1 * count_rectX;
-                    if (x > x1 * rectX && x <= x1 * rectX + rectX && y > y1 * rectY && y <= y1 * rectY + rectY) {
+                    if (x >= x1 * rectX && x < x1 * rectX + rectX && y >= y1 * rectY && y < y1 * rectY + rectY) {
                         lists[index].Add(c.R);
                     }
-                        
+
                 }
             }
-          
 
-            for (int y = 0;  y < count_rectY; y++) {
+
+            for (int y = 0; y < count_rectY; y++) {
                 for (int x = 0; x < count_rectX; x++) {
-                    int loc = x + y * count_rectX;          
-                    richTextBox1.AppendText(Math.Round(lists[loc].Average()).ToString() + "   ");
-                    
+                    int loc = x + y * count_rectX;
+                    double temp = Math.Round(lists[loc].Average());
+                    richTextBox1.AppendText(temp.ToString() + "   ");
+                    result.Add((int) temp);
                 }
                 richTextBox1.AppendText("\n");
 
             }
-          
-          
+            return result;
+
         }
 
+        private void pixelization (List<int> value, Bitmap img, int count_rectX, int count_rectY) {
+            Bitmap result = new Bitmap(img.Width, img.Height);
+            int x1, y1;
+            int rectX = img.Width / count_rectX;
+            int rectY = img.Height / count_rectY;
+            for (int x = 0; x < rectX * count_rectX; x++) {
+                for (int y = 0; y < rectY * count_rectY; y++) {
+                    x1 = x / rectX;
+                    y1 = y / rectY;
+                    int index = x1 + y1 * count_rectX;
+                    if (x >= x1 * rectX && x < x1 * rectX + rectX && y >= y1 * rectY && y < y1 * rectY + rectY) {
+                        result.SetPixel(x, y, Color.FromArgb(img.GetPixel(x, y).A, value[index], value[index], value[index]));
+
+                    }
+
+                }
+            }
+            pictureBox3.Image = result;
+        }
+        private void scaleAndShifting (List<int> avr_gray_value) {
+           
+
+            int old_min_value = avr_gray_value.Min();
+            int old_max_value = avr_gray_value.Max();
+
+            double scale = ( 0.9 - 0.1 ) / ( old_max_value - old_min_value );
+
+            List<double> scaled_gray = new List<double>();
+            for (int i = 0; i < avr_gray_value.Count; i++) {
+                scaled_gray.Add(avr_gray_value[i] * scale);
+            }
+            double new_min_value = scaled_gray.Min();
+            double new_max_value = scaled_gray.Max();
+
+            double shift = 0.1 - new_min_value;
+
+
+            for (int i = 0; i < scaled_gray.Count; i++) {
+                scaled_gray[i] = avr_gray_value[i] * scale + shift;
+            }
+            richTextBox1.AppendText(" ---------------\n");
+            for (int i = 0; i < scaled_gray.Count; i++) {
+                richTextBox1.AppendText(scaled_gray[i].ToString() + "\n");
+            }
+        }
+
+
         private void button3_Click (object sender, EventArgs e) {
+            richTextBox1.Clear();
             int x = Int32.Parse(textBox1.Text);
             int y = Int32.Parse(textBox2.Text);
-            pixialization(img_gray,x,y);
+            if (x >= img.Width) {
+                x = img.Width;
+            }
+            if (y >= img.Height) {
+                y = img.Height;
+            }
+            List<int> avr_gray_value = new List<int>();
+            avr_gray_value = AverageGray(img_gray, x, y);
+            pixelization(avr_gray_value, img_gray, x, y);
+
+            scaleAndShifting(avr_gray_value);
+
+
         }
 
         private void Form1_Load (object sender, EventArgs e) {
